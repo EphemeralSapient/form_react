a911aa64a8a2456e8eb702047b022b086393ba00
EphemeralSapient <nowayofway@gmail.com>
2023-07-28 14:09:38 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import VotingListForm from './valid';
 import StatusPage from './StatusPage';
 import { AppProvider } from './AppContext'; // Import the AppProvider
