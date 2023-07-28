65ffae68e208eadcbffc7e9c7f38da619dbacfd1
EphemeralSapient <nowayofway@gmail.com>
2023-07-28 13:36:08 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import VotingListForm from './valid';
+import StatusPage from './StatusPage';
+import { AppProvider } from './AppContext'; // Import the AppProvider
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppProvider>
+      <Routes>
+        <Route path="/" element={<VotingListForm/>} />
+        <Route path="/status" element={<StatusPage />} /> 
+      </Routes>
+    </AppProvider>
+    </Router>
   );
-}
+};
 
 export default App;
